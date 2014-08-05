1cdeed25674ff32198aec86615a30bb7553aaf69
Maximilian Maroe <main@makeagamenow.com>
2014-08-04 21:30:43 -0700

    Enemy's asplode

---
+++ Assets/Scripts/GibOnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibOnTrigger : MonoBehaviour
+{
+	public GameObject gib;
+
+	void OnTriggerEnter()
+	{
+		Instantiate(gib, transform.position, Quaternion.identity);
+		Destroy(gameObject);
+	}
+}
