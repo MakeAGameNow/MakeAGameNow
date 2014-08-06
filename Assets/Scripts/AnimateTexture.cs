2d2c140420820d16d7ddbe783f0416878d0a16af
Maximilian Maroe <main@makeagamenow.com>
2014-08-05 19:28:39 -0700

    Animated texture "movement"

---
+++ Assets/Scripts/AnimateTexture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateTexture : MonoBehaviour
+{
+	public Vector2 speed = Vector2.one;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		renderer.material.mainTextureOffset += speed * Time.deltaTime;
+	}
+}
