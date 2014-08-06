a6b5cfaa603bf28c7cb3ebbbf8287bdedc147552
Maximilian Maroe <main@makeagamenow.com>
2014-08-05 23:32:53 -0700

    Pillar scale test

--- Assets/Scripts/ScaleOverTime.cs
+++ Assets/Scripts/ScaleOverTime.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ScaleOverTime : MonoBehaviour {
+public class ScaleOverTime : MonoBehaviour
+{
+	public Vector3 finalScale = Vector3.zero;
+	public float time = 1.0f;
+
+	private Vector3 initialScale;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		initialScale = transform.localScale;
+		StartCoroutine("Scale");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	IEnumerator Scale()
+	{
+		float t = 0.0f;
+
+		while(t <= time)
+		{
+			transform.localScale = Vector3.Lerp(initialScale,finalScale,t/time);
+			t += Time.deltaTime;
+			yield return null;
+		}
+
+		transform.localScale = finalScale;
 	}
 }
