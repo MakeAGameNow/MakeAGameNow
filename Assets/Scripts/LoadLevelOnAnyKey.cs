b632acdd1d784906953bbd74c534801032eeeb52
Maximilian Maroe <main@makeagamenow.com>
2014-08-06 22:42:48 -0700

    Title screen to game flow

---
+++ Assets/Scripts/LoadLevelOnAnyKey.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelOnAnyKey : MonoBehaviour
+{
+	public string levelName;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.anyKeyDown)
+		{
+			Application.LoadLevel(levelName);
+		}
+	}
+}
