2b928f45e855ecd62ef688386e13383bcb4313c5
Maximilian Maroe <main@makeagamenow.com>
2014-08-06 18:51:10 -0700

    It's a game!

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	private bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(GameObject.FindGameObjectWithTag("Player") == null)
+		{
+			gameOver = true;
+		}
+
+		if(gameOver)
+		{
+			if(Input.anyKeyDown)
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		if(gameOver == true)
+		{
+			GUILayout.Label("Game Over!  Press any key to reset!");
+		}
+	}
+}
