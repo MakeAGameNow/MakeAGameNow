9211f380e3b5b85b9121df3aa940b9085d472c3c
Maximilian Maroe <main@makeagamenow.com>
2014-08-06 19:16:53 -0700

    Score system, and unified speed system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+	public static GameManager Instance;
+
+	public float pointsPerUnitTravelled = 1.0f;
+	public float gameSpeed = 10.0f;
+
+	private float score = 0.0f;
+	private static float highScore = 0.0f;
 	private bool gameOver = false;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		Instance = this;
 	}
 	
 	// Update is called once per frame
@@ -25,10 +33,23 @@ public class GameManager : MonoBehaviour
 				Application.LoadLevel(Application.loadedLevel);
 			}
 		}
+
+		if(!gameOver)
+		{
+			score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
+			if(score > highScore)
+			{
+					highScore = score;
+			}
+		}
 	}
 
 	void OnGUI()
 	{
+		int currentScore = (int)score;
+		int currentHighScore = (int)highScore;
+		GUILayout.Label("Score: " + currentScore.ToString());
+		GUILayout.Label("Highscore: " + currentHighScore.ToString());
 		if(gameOver == true)
 		{
 			GUILayout.Label("Game Over!  Press any key to reset!");
---
+++ Assets/Scripts/MoveBasedOnGameSpeed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBasedOnGameSpeed : MonoBehaviour
+{
+	public Vector3 direction = Vector3.forward;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += transform.rotation*(direction.normalized*GameManager.Instance.gameSpeed*Time.deltaTime);
+	}
+}
