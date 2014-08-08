28503a15ecd60e51aad1c370cb8dee903320b5c9
Maximilian Maroe <main@makeagamenow.com>
2014-08-07 23:27:11 -0700

    MUSIC VISUALIZER FIRST PASS LAST PASS

---
+++ Assets/Scripts/FogBeat.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogBeat : MonoBehaviour
+{
+	public int samples = 64;
+	public int channel = 0;
+	public int frequencyChannel = 32;
+	public float amplitudeMultiplier = 1.0f;
+	public FFTWindow window;
+
+	private float originalFogDensity;
+
+	// Use this for initialization
+	void Start ()
+	{
+		originalFogDensity = RenderSettings.fogDensity;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float[] data = new float[samples];
+		AudioListener.GetSpectrumData (data, channel, window);
+		RenderSettings.fogDensity = originalFogDensity + data[frequencyChannel]*amplitudeMultiplier;
+	}
+}
