59a3c1cc83147e51337934dfe773168a2b5a4e98
Maximilian Maroe <main@makeagamenow.com>
2014-08-07 23:05:24 -0700

    Color cycle now speed based.  Some shader junk, and an additive grid layer.

--- Assets/Scripts/CycleFogColor.cs
+++ Assets/Scripts/CycleFogColor.cs
@@ -3,31 +3,25 @@ using System.Collections;
 
 public class CycleFogColor : MonoBehaviour
 {
-	public float cycleTime = 30.0f;
+	public float speed = 0.1f;
+	public float hue = 0.0f;
 	public float saturation = 1.0f;
 	public float brightness = 1.0f;
 
-	// Use this for initialization
-	void Start ()
+	void Update()
 	{
-		StartCoroutine("CycleColors");
-	}
+		hue += speed*Time.deltaTime;
 
-	IEnumerator CycleColors()
-	{
-		float t = 0.0f;
-		float hue = 0.0f;
-
-		while(t <= cycleTime)
+		while(hue > 1.0f)
 		{
-			hue = Mathf.Lerp(0.0f,1.0f,t/cycleTime);
-			RenderSettings.fogColor = new HSBColor(hue,saturation,brightness).ToColor();
-			t += Time.deltaTime;
-			yield return null;
+			hue -= 1.0f;
 		}
-		
-		hue = 1.0f;
 
-		StartCoroutine("CycleColors");
+		while(hue < 0.0f)
+		{
+			hue += 1.0f;
+		}
+
+		RenderSettings.fogColor = new HSBColor (hue, saturation, brightness).ToColor();
 	}
 }
--- Assets/Scripts/CycleMaterialColor.cs
+++ Assets/Scripts/CycleMaterialColor.cs
@@ -3,31 +3,27 @@ using System.Collections;
 
 public class CycleMaterialColor : MonoBehaviour
 {
-	public float cycleTime = 30.0f;
+	public string colorPropertyName = "_Color";
+	public float speed = 0.1f;
+	public float hue = 0.0f;
 	public float saturation = 1.0f;
 	public float brightness = 1.0f;
-
-	// Use this for initialization
-	void Start ()
+	
+	void Update()
 	{
-		StartCoroutine("CycleColors");
-	}
-
-	IEnumerator CycleColors()
-	{
-		float t = 0.0f;
-		float hue = 0.0f;
-
-		while(t <= cycleTime)
+		hue += speed*Time.deltaTime;
+		
+		while(hue > 1.0f)
 		{
-			hue = Mathf.Lerp(0.0f,1.0f,t/cycleTime);
-			renderer.material.color = new HSBColor(hue,saturation,brightness).ToColor();
-			t += Time.deltaTime;
-			yield return null;
+			hue -= 1.0f;
 		}
 		
-		hue = 1.0f;
+		while(hue < 0.0f)
+		{
+			hue += 1.0f;
+		}
 
-		StartCoroutine("CycleColors");
+
+		renderer.material.SetColor(colorPropertyName, new HSBColor (hue, saturation, brightness).ToColor());
 	}
 }
