ded272b08cf4a6636046f8d6f19405e114a6d8f7
Maximilian Maroe <main@makeagamenow.com>
2014-08-07 21:29:32 -0700

    ALL THE PRETTY COLORS!

---
+++ Assets/Scripts/CycleFogColor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CycleFogColor : MonoBehaviour
+{
+	public float cycleTime = 30.0f;
+	public float saturation = 1.0f;
+	public float brightness = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine("CycleColors");
+	}
+
+	IEnumerator CycleColors()
+	{
+		float t = 0.0f;
+		float hue = 0.0f;
+
+		while(t <= cycleTime)
+		{
+			hue = Mathf.Lerp(0.0f,1.0f,t/cycleTime);
+			RenderSettings.fogColor = new HSBColor(hue,saturation,brightness).ToColor();
+			t += Time.deltaTime;
+			yield return null;
+		}
+		
+		hue = 1.0f;
+
+		StartCoroutine("CycleColors");
+	}
+}
