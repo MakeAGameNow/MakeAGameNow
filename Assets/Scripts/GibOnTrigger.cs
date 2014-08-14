140d3d23ef21265ddd4edc0db0e241521c304284
Maximilian Maroe <main@makeagamenow.com>
2014-08-13 21:56:38 -0700

    Collectable stars!  YAY!  (They still need their own particle effect

--- Assets/Scripts/GibOnTrigger.cs
+++ Assets/Scripts/GibOnTrigger.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class GibOnTrigger : MonoBehaviour
 {
 	public GameObject gib;
+	public float scoreValue = 0.0f;
 
 	void OnTriggerEnter()
 	{
 		Instantiate(gib, transform.position, Quaternion.identity);
+		GameManager.Instance.score += scoreValue;
 		Destroy(gameObject);
 	}
 }
