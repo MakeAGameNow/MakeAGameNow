effec54882e6bb10b65f84d7bf30a8b50933dd6f
Maximilian Maroe <main@makeagamenow.com>
2014-08-14 19:25:18 -0700

    Bug fixes last minute

---
+++ Assets/Scripts/DestroyParticleWhenDone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticleWhenDone : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+	{
+		if(!particleSystem.isPlaying)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
